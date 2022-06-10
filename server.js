0a51f7a37382d9ec5bc4533cc1b438051cacc59b
SwapnilVedpathak-sv <swapnilvedpathak.sv@gmail.com>
2022-06-10 15:47:14 +0530

    wrap up

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 let express = require("express");
 let app = express();
-app.use(express.static(__dirname + "/dist/vsfc-v1"));
+app.use(express.static(__dirname + "/dist/vsfc_v1"));
 
 app.get('/*', (req, res) => {
-    res.sendFile(__dirname + "/dist/vsfc-v1/index.html");
+    res.sendFile(__dirname + "/dist/vsfc_v1/index.html");
 })
 
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 8080);
